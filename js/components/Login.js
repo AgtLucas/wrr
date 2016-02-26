b4695b7ed2d296a14a72ae755e558ffeef92f1ff
Lucas <AgtLucas@users.noreply.github.com>
2016-02-26 14:59:58 -0300

    Update Login.js

--- js/components/Login.js
+++ js/components/Login.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
 
 let Login = React.createClass({
 
   render() {
-    return (<div>Welcome to Login!</div>);
+    return (<div>Welcome to Login!</div>)
   }
-});
+})
 
-export default Login;
+export default Login
